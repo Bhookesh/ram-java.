912b9b9923d8c9be9f461d639f4d87b2641902a8
Bhookesh <bhookeshkumar529@gmail.com>
2024-06-24 11:24:08 +0530

    Add files via upload

---
+++ Object1.java
@@ -0,0 +1,17 @@
+/**
+ * Mahesh
+ */
+public class Mahesh {
+  void fun1() {
+    System.out.println("fun1");
+  }
+
+  void fun2{
+    System.out.println("fun2")
+  }
+}
+
+public class Object1 {
+public static void main(String []args)
+  
+}
